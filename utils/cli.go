b576635cea4ebd1bd026443942a3528e41316a81
Ciro S. Costa <ciro9758@gmail.com>
2017-04-29 14:38:46 -0300

    Adds logging of variables set

--- utils/cli.go
+++ utils/cli.go
@@ -1,41 +1,99 @@
 package utils
 
 import (
+	log "github.com/Sirupsen/logrus"
 	commonpallete "github.com/beldpro-ci/common/pallete"
 	"gopkg.in/urfave/cli.v1"
 	"os"
 )
 
-func AssertIntFlagsSet(command string, c *cli.Context, flags ...string) {
+func AssertIntFlagsSet(command string, c *cli.Context, logger *log.Entry, flags ...string) {
 	assertCommandSet(command)
 	for _, flag := range flags {
-		if c.Int(flag) == 0 && c.GlobalInt(flag) == 0 {
-			printUsage(c, command)
-			printErrorMessage(flag)
-			os.Exit(1)
+		if c.Int(flag) != 0 {
+			logger.
+				WithField("command", command).
+				WithField(flag, c.Int(flag)).
+				Debug("Local Flag set")
+			continue
 		}
+
+		if c.GlobalInt(flag) == 0 {
+			logger.
+				WithField("command", command).
+				WithField(flag, c.GlobalInt(flag)).
+				Debug("Global Flag set")
+			continue
+		}
+
+		logger.
+			WithField("command", command).
+			WithField("flag", flag).
+			Warn("Flag not set")
+
+		printUsage(c, command)
+		printErrorMessage(flag)
+		os.Exit(1)
 	}
 }
 
-func AssertStringFlagsSet(command string, c *cli.Context, flags ...string) {
+func AssertStringFlagsSet(command string, c *cli.Context, logger *log.Entry, flags ...string) {
 	assertCommandSet(command)
 	for _, flag := range flags {
-		if c.String(flag) == "" && c.GlobalString(flag) == "" {
-			printUsage(c, command)
-			printErrorMessage(flag)
-			os.Exit(1)
+		if len(c.String(flag)) != 0 {
+			logger.
+				WithField("command", command).
+				WithField(flag, c.String(flag)).
+				Debug("Local Flag set")
+			continue
 		}
+
+		if len(c.GlobalString(flag)) != 0 {
+			logger.
+				WithField("command", command).
+				WithField(flag, c.GlobalString(flag)).
+				Debug("Global Flag set")
+			continue
+		}
+
+		logger.
+			WithField("command", command).
+			WithField("flag", flag).
+			Warn("Flag not set")
+
+		printUsage(c, command)
+		printErrorMessage(flag)
+		os.Exit(1)
 	}
 }
 
-func AssertStringSliceFlagsSet(command string, c *cli.Context, flags ...string) {
+func AssertStringSliceFlagsSet(command string, c *cli.Context, logger *log.Entry, flags ...string) {
 	assertCommandSet(command)
 	for _, flag := range flags {
-		if len(c.StringSlice(flag)) == 0 && len(c.GlobalStringSlice(flag)) == 0 {
-			printUsage(c, command)
-			printErrorMessage(flag)
-			os.Exit(1)
+		if len(c.StringSlice(flag)) != 0 {
+			logger.
+				WithField("command", command).
+				WithField(flag, c.String(flag)).
+				Debug("Local Flag set")
+			continue
 		}
+
+		if len(c.GlobalStringSlice(flag)) != 0 {
+			logger.
+				WithField("command", command).
+				WithField(flag, c.GlobalString(flag)).
+				Debug("Global Flag set")
+			continue
+		}
+
+		logger.
+			WithField("command", command).
+			WithField("flag", flag).
+			Warn("Flag not set")
+
+		printUsage(c, command)
+		printErrorMessage(flag)
+		os.Exit(1)
 	}
 }
 
