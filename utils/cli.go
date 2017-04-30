c0bd914a7d483c6582a997fc51ca3fa28b7a4292
Ciro S. Costa <ciro9758@gmail.com>
2017-04-30 09:10:15 -0300

    Adds assert for int64 type

--- utils/cli.go
+++ utils/cli.go
@@ -37,6 +37,36 @@ func AssertIntFlagsSet(command string, c *cli.Context, logger *log.Entry, flags 
 	}
 }
 
+func AssertInt64FlagsSet(command string, c *cli.Context, logger *log.Entry, flags ...string) {
+	assertCommandSet(command)
+	for _, flag := range flags {
+		if c.Int64(flag) != 0 {
+			logger.
+				WithField("command", command).
+				WithField(flag, c.Int64(flag)).
+				Debug("Local Flag set")
+			continue
+		}
+
+		if c.GlobalInt64(flag) == 0 {
+			logger.
+				WithField("command", command).
+				WithField(flag, c.GlobalInt64(flag)).
+				Debug("Global Flag set")
+			continue
+		}
+
+		logger.
+			WithField("command", command).
+			WithField("flag", flag).
+			Warn("Flag not set")
+
+		printUsage(c, command)
+		printErrorMessage(flag)
+		os.Exit(1)
+	}
+}
+
 func AssertStringFlagsSet(command string, c *cli.Context, logger *log.Entry, flags ...string) {
 	assertCommandSet(command)
 	for _, flag := range flags {
